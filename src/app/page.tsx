e5cdc6243d7e1a4d658962b217b354bd934fcd87
Clement Murphy <clemmurphy@gmail.com>
2023-07-11 20:40:01 +0100

    add font and styled-components, remove lots of template css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import styles from './page.module.css'
+import { explorerFont, orbiterFont } from './layout'
+import Blob from 'components/atoms/blob'
 
-export default async function Home() {
+export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>Hello, World!</div>
+    <main>
+      <div className={orbiterFont.variable}>
+        <h1>Clement Murphy</h1>
+      </div>
+      <div className={explorerFont.variable}>
+        <p>Product Manager</p>
+      </div>
+      <div style={{ display: 'flex', gap: '4px' }}>
+        <Blob style={{ backgroundColor: 'var(--color-pink)' }} />
+        <Blob style={{ backgroundColor: 'var(--color-yellow)' }} />
+        <Blob style={{ backgroundColor: 'var(--color-blue)' }} />
+        <Blob style={{ backgroundColor: 'var(--color-green)' }} />
+      </div>
     </main>
   )
 }
---
+++ src/components/atoms/blob.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import styled from 'styled-components'
+
+const Blob = styled.div`
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+`
+
+export default Blob
